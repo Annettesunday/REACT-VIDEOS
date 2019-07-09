8060b9af1817858aa32f7e90b8c2b3bd9e125cf2
annettesunday <annette.odhiambo@andela.com>
2019-07-09 16:14:03 +0300

    ch(App): deeply nested callbacks

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,8 @@ import VideoList from "./VideoList";
 
 class App extends React.Component {
   state = {
-    videos: []
+    videos: [],
+    selectedVideo: null
   };
   onTermSubmit = async term => {
     const response = await youtube.get("/search", {
@@ -15,11 +16,18 @@ class App extends React.Component {
     });
     this.setState({ videos: response.data.items });
   };
+
+  onVideoSelect = video => {
+    console.log("Fromt the app", video);
+  };
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos}/>
+        <VideoList
+          videos={this.state.videos}
+          onVideoSelect={this.onVideoSelect}
+        />
       </div>
     );
   }
