6ff590a89a3962c351a17f5a730639fc51368626
annettesunday <annette.odhiambo@andela.com>
2019-07-09 16:54:46 +0300

    ch(App): defaulting video selection

--- src/components/App.js
+++ src/components/App.js
@@ -9,13 +9,20 @@ class App extends React.Component {
     videos: [],
     selectedVideo: null
   };
+
+  componentDidMount() {
+    this.onTermSubmit("buildings");
+  }
   onTermSubmit = async term => {
     const response = await youtube.get("/search", {
       params: {
         q: term
       }
     });
-    this.setState({ videos: response.data.items });
+    this.setState({ 
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+     });
   };
 
   onVideoSelect = video => {
